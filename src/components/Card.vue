76ecb21b5c44a8713fdbadb555c5d7feacf6281a
Hrezontem <nikitatarapata@gmail.com>
2024-05-29 16:42:24 +0300

    Вёрстка главной страницы закончена

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -17,13 +17,15 @@ export default {
 <template>
   <div class="card">
     <span class="card__header__text">{{card.name}}</span>
-    <div class="card__text">{{card.price}} ₽/мес.</div>
-    <span class="card__text">
+    <div class="card__container">
+      <div class="card__text">{{card.price}} ₽/мес.</div>
+      <span class="card__text">
       Ядра    {{card.cores}}<br>
       Память  {{card.memory}}<br>
       Диски   {{card.drive}}
     </span>
-    <button-u-i>Заказать сейчас</button-u-i>
+      <button-u-i class="btn-card">Заказать сейчас</button-u-i>
+    </div>
   </div>
 </template>
 
@@ -38,17 +40,27 @@ export default {
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.32);
 }
 .card__header__text{
+  align-self: flex-start;
   font-weight: bold;
   font-size: 24px;
 }
 .card__text{
-  margin-top: 92px;
 }
-.card__text:nth-child(2){
+.card__text:nth-child(1){
   font-size: 22px;
 }
-.card__text:nth-child(3){
+.card__container > .card__text:nth-child(2){
   font-size: 15px;
-  margin-bottom: 44px;
+}
+.card__container{
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: column;
+  gap: 65px;
+
+}
+.btn-card{
+  align-self: flex-end;
 }
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -35,13 +35,29 @@ export default {
         :card="card"
         v-for="card in cards"
     >
-
     </card>
+  </div>
+  <div class="main__text">
+    <span>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </span>
+    <span>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </span>
   </div>
   <footer-component></footer-component>
 </template>
 
 <style>
+.main__text{
+  width: 60vw;
+  display: flex;
+  flex-direction: column;
+  gap: 64px;
+  font-size: 16px;
+}
 .cards{
   display: flex;
   align-items: center;
